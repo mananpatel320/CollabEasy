75cfc903e3d09c4918b20107c4c35bfc5ed8968f
ManaN Patel <mananpatel320@gmail.com>
2021-12-20 15:57:42 +0530

    Added Title

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Spacer, Button } from './utilities';
+import { Spacer } from './utilities';
+import mainLogo from './teamwork.png';
 
 export class Cover extends Component {
     render() {
@@ -9,22 +10,13 @@ export class Cover extends Component {
                 ...this.props.style
             }} className='primary'>
                 <div style={{ fontSize: '20px' }} className='flexbox cu vertical full'>
-                {/* <h1>DrawBoard</h1> */}
-                    <div>
-                        <Button href='https://github.com/umerkk164/Drawboard/' classes='white'>GitHub</Button>
-                        <Button href='http://github.com/umerkk164' classes='white'>Creator</Button>
-                    </div>
+                    <h1><img src={mainLogo} width="42" height="42"></img>     Collab<strong>Easy</strong></h1>
 
                     <Spacer height='1rem'></Spacer>
 
-                    <h1>Draw anything.</h1>
-                    <h1>Collaborate on ideas.</h1>
-                    <h1>But, together.</h1>
-
                     <Spacer height='6rem'></Spacer>
 
-                    <span style={{ fontSize: '0.8em' }}>This application is built using the MERN stack, with MongoDB, Express, React and Node.js. This instance is a project demonstration only and not meant for production.</span>
-                    <span style={{ fontSize: '0.8em' }} className='mt-1'>It utilises socket.io for real-time drawing and chat messages, and <a href='https://umerkk164.github.io/needlessjs/'>needlessjs</a> for working with HTML canvases</span>
+                    <span style={{ fontSize: '0.8em' }}>A platform for you to collaborate, learn and grow together. Problem based learning helps learners to solve questions in a group using drawboard.</span>
                 </div>
             </div>
         )
